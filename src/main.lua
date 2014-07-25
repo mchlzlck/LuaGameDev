fe9236335fcd57115c5b9378db9e078ee94f5237
Michael Ziluck <plugindev68@gmail.com>
2014-07-25 10:58:25 -0400

    More stuff
    Added comments
    Added borders
    Reduced enemy spawn rate
    Added bullets

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,4 @@
+-- Function called when the game starts
 function love.load()
 
 	-- Window data
@@ -12,6 +13,10 @@ function love.load()
 	-- Game data
 	Game = {}
 	Game.playing = true
+	Game.stars= {}
+	for i=0,30 do
+		table.insert(Game.stars, {x = love.math.random(0,1280), y = love.math.random(0, 720)})
+	end
 	Game.tick = 0
 
 	-- Menu data
@@ -29,23 +34,27 @@ function love.load()
 	Images.menu.scores_red = love.graphics.newImage("scores_red.png")
 	Images.menu.quit_white = love.graphics.newImage("quit_white.png")
 	Images.menu.quit_red = love.graphics.newImage("quit_red.png")
-	Images.game.one = love.graphics.newImage("one.png")
-	Images.game.two = love.graphics.newImage("two.png")
-	Images.game.three = love.graphics.newImage("three.png")
-	Images.game.four = love.graphics.newImage("four.png")
-	Images.game.five = love.graphics.newImage("five.png")
+	Images.game.one = love.graphics.newImage("one.gif")
+	Images.game.two = love.graphics.newImage("two.gif")
+	Images.game.three = love.graphics.newImage("three.gif")
+	Images.game.four = love.graphics.newImage("four.gif")
+	Images.game.five = love.graphics.newImage("five.gif")
 
 	-- Player data
 	Player = {}
 	Player.x = Window.width * .1
 	Player.y = Window.height * .5
+	Player.bullets = {}
 	Player.image = love.graphics.newImage("player.png")
 
 	-- Enemy data
 	EnemyArray = {}
 end
 
+-- Draw the game
 function love.draw()
+
+	-- Draw the menu options
 	if Program.state == "menu" then
 		love.graphics.draw(Images.menu.title, Window.width / 2 - 691 / 2, Window.height * .075)
 		if Menu.selected == 0 then
@@ -64,37 +73,75 @@ function love.draw()
 			love.graphics.draw(Images.menu.quit_white, Window.width / 2 - 100 / 2, Window.height * .75)
 		end
 	end
+
+	-- Draw the game
 	if Program.state == "play" then
+
+		-- When the game is paused
 		if not Game.playing then
 			love.graphics.print("Quit (Q)", 50, 50)
 			love.graphics.print("Resume(Esc)", 50, 75)
 		end
+
+		-- Draw enemies
 		for index,Enemy in ipairs(EnemyArray) do
 			love.graphics.draw(Enemy.image, Enemy.x, Enemy.y)
 		end
+		
+		-- Draw player
 		love.graphics.draw(Player.image, Player.x, Player.y)
+		
+		-- Draw bullets
+		for index,Bullet in ipairs(Player.bullets) do
+			love.graphics.circle("fill", Bullet.x, Bullet.y, 3, 100)
+		end
+		
+		-- Draw stars
+		love.graphics.setColor(255,248,153)
+		for index,Star in ipairs(Game.stars) do
+			love.graphics.circle("fill", Star.x, Star.y, 1)
+		end
+		love.graphics.setColor(255,255,255)
 	end
 end
 
+-- Update the values
 function love.update(dt)
+
+	-- When the game is not paused
 	if(Game.playing) then
+		-- Move the player
 		if(love.keyboard.isDown("w")) or (love.keyboard.isDown("up")) then
-			Player.y = Player.y - 3
+			if Player.y >= 0 then
+				Player.y = Player.y - 3
+			end
 		end
 		if(love.keyboard.isDown("s")) or (love.keyboard.isDown("down")) then
-			Player.y = Player.y + 3
+			if Player.y <= Window.height - 40 then
+				Player.y = Player.y + 3
+			end
 		end
+
+		-- Tick tracking
 		Game.tick = Game.tick + 1
-		if(Game.tick >= 60) then
+		if(Game.tick >= 120) then
 			spawnEnemy()
 			Game.tick = 0
 		end
+
+		-- Do the enemy stuff
 		adjustImages()
 		moveEnemies()
+		
+		-- Do bullet stuff
+		moveBullets()
 	end
 end
 
+-- Get the pressed keys
 function love.keypressed(key, ispressed)
+
+	-- When the user is at the menu
 	if Program.state == "menu" then
 		if key == "w" or key == "up" then
 			if Menu.selected == 0 then
@@ -120,7 +167,12 @@ function love.keypressed(key, ispressed)
 			end
 		end
 	end
+
+	-- When the game is running
 	if Program.state == "play" then
+		if(key == " ") then
+			fireBullet()
+		end
 		if(key == "escape") then
 			Game.playing = not Game.playing
 		end
@@ -130,10 +182,12 @@ function love.keypressed(key, ispressed)
 	end
 end
 
+-- Add a new enemy to the game
 function spawnEnemy()
 	table.insert(EnemyArray,{x = 1280, y = love.math.random(0,680), health = love.math.random(1,5), image = nil})
 end
 
+-- Assign the images to the enemies
 function adjustImages()
 	if (EnemyArray ~= nil) then
 		for index,Enemy in ipairs(EnemyArray) do
@@ -147,6 +201,7 @@ function adjustImages()
 	end
 end
 
+-- Move all of the enemies on the screen
 function moveEnemies()
 	for index,Enemy in ipairs(EnemyArray) do
 		if(Enemy.x <= -32) then
@@ -154,5 +209,20 @@ function moveEnemies()
 		else
 			Enemy.x = Enemy.x - 3
 		end
+	end
+end
+
+-- Fire a bullet
+function fireBullet()
+	table.insert(Player.bullets,{x = Player.x + 40, y = Player.y + 20})
+end
+
+-- Move bullets
+function moveBullets()
+	for index,Bullet in ipairs(Player.bullets) do
+		if(Bullet.x >= 1280) then
+			table.remove(Player.bullets,index)
+		end
+		Bullet.x = Bullet.x + 4
 	end
 end
